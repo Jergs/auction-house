1eb81777783124b415beb93a1320169354b7c519
Yehor Havryliuk <yehor.havryliuk@gmail.com>
2022-08-22 22:03:52 +0300

    Added listed data

--- auction-house-web/src/main/java/com/demo/auctionhouse/controller/LotController.java
+++ auction-house-web/src/main/java/com/demo/auctionhouse/controller/LotController.java
@@ -1,14 +1,21 @@
 package com.demo.auctionhouse.controller;
 
+import com.demo.auctionhouse.service.LotService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/lots")
+@AllArgsConstructor
 public class LotController {
 
+    private final LotService lotService;
+
     @RequestMapping({"/", ""})
-    public String listLots() {
+    public String listLots(Model model) {
+        model.addAttribute("lots", lotService.findAll());
         return "/lots/index";
     }
 }
